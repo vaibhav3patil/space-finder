cc1a6c945ecf6a107ef73de9fe1a6a9bb34006d1
vaibhav patil <vaibhav.patil@asurion.com>
2024-01-26 17:24:00 +0530

    use aws lambda to access s3 buckets using aws-sdk/client

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,10 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
-import { v4 } from 'uuid'
- 
+import {S3Client, ListBucketsCommand} from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({});
+  
 async function handler(event:APIGatewayProxyEvent, context: Context){
+
+    // in order to access the s3 bucket from lambda we need to add policy to access s3 buckets in lambda stack
+    const command = new ListBucketsCommand({});
+    const listBucketResult = (await s3Client.send(command)).Buckets;
+
+
     const repsonse : APIGatewayProxyResult = {
         statusCode: 200,
-        body: JSON.stringify("hello fron lambda, this is the id:" + v4())
+        body: JSON.stringify("hello fron lambda, here are your bucket:" + JSON.stringify(listBucketResult))
     }
     console.log(repsonse);
     return repsonse;
