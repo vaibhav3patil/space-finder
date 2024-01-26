b9dfeb7a793f27f7999165385f41b1dc86e40dc9
vaibhav patil <vaibhav.patil@asurion.com>
2024-01-26 23:07:28 +0530

    dynamodb putItem operation using Lambda and apigateway

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -1,18 +1,34 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { postSpaces } from "./PostSpaces";
   
+
+const ddbClient = new DynamoDBClient({})
+
 async function handler(event:APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>{
 
     let message: string;
-    switch (event.httpMethod) {
-        case 'GET':
-            message = "hello from GET!"
-            break;
-        case 'POST':
-            message = "hello from POST!"
-            break;
-        default:
-            break;
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                message = "hello from GET!"
+                break;
+            case 'POST':
+                const response = postSpaces(event, ddbClient);
+                return response;
+            default:
+                break;
+        }
+    } catch (error) {
+        console.error(error)
+        return {
+            statusCode:500,
+            body: JSON.stringify(error.message)
+        }
     }
+
+   
+
     const repsonse : APIGatewayProxyResult = {
         statusCode: 200,
         body: JSON.stringify(message)
