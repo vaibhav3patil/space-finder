45bd7abe586caa54d634ff39a43915e150fccf8f
vaibhav patil <vaibhav.patil@asurion.com>
2024-01-26 23:48:20 +0530

    marshalling with DynamodbDocumentClient

--- src/services/spaces/PostSpacesWithDoc.ts
+++ src/services/spaces/PostSpacesWithDoc.ts
@@ -1,4 +1,5 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 } from "uuid";
 
@@ -6,20 +7,13 @@ import { v4 } from "uuid";
 
 export async function postSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult>{
 
+    const dddocClient = DynamoDBDocumentClient.from(ddbClient);
     const randomId = v4();
     const item = JSON.parse(event.body);
 
-    const result = await ddbClient.send(new PutItemCommand({
+    const result = await dddocClient.send(new PutItemCommand({
         TableName: process.env.TABLE_NAME,
-        
-        Item: {
-            id: {
-                S: randomId,
-            },
-            location: {
-                S: item.location
-            }
-        }
+        Item: item
     }));
     console.log(result);
     return {
